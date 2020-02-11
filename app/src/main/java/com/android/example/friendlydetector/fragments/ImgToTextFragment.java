1cecffd191d51d3e8a306425e2a3597dd596f13f
Trinh Dung <c170017@e.ntu.edu.sg>
2020-02-11 22:33:04 +0800

    Load img from gallery and scale image down

--- app/src/main/java/com/android/example/friendlydetector/fragments/ImgToTextFragment.java
+++ app/src/main/java/com/android/example/friendlydetector/fragments/ImgToTextFragment.java
@@ -1,18 +1,34 @@
 package com.android.example.friendlydetector.fragments;
 
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.android.example.friendlydetector.R;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static android.app.Activity.RESULT_OK;
+
 
 public class ImgToTextFragment extends Fragment {
+    private Button loadButton;
+    private ImageView image;
     private ImgToTextFragment() {
 
     }
@@ -32,9 +48,48 @@ public class ImgToTextFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_img_to_text, container, false);
+        View view = inflater.inflate(R.layout.fragment_img_to_text, container, false);
+        loadButton = view.findViewById(R.id.load);
+        image = view.findViewById(R.id.image_holder);
+
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loadIntent = new Intent(
+                        Intent.ACTION_PICK);
+                loadIntent.setType("image/*");
+                startActivityForResult(loadIntent, 1);
+            }
+        });
+
+        return view;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && null != data) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContext().getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+
+                //get screen width
+                DisplayMetrics displayMetrics = new DisplayMetrics();
+                getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                int screenWidth = displayMetrics.widthPixels;
+
+                //scale the image size down
+                int height = screenWidth * selectedImage.getHeight() / selectedImage.getWidth();
+                Bitmap scaledImage = Bitmap.createScaledBitmap(selectedImage, screenWidth, height, true);
+
+                image.setImageBitmap(scaledImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     public void onAttach(Context context) {
