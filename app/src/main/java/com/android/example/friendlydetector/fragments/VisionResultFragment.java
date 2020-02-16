3fa70e391fcbb65caee1eea203b144fc3e8b537c
Trinh Dung <c170017@e.ntu.edu.sg>
2020-02-16 15:09:30 +0800

    Init Vision Result fragment

---
+++ app/src/main/java/com/android/example/friendlydetector/fragments/VisionResultFragment.java
@@ -0,0 +1,50 @@
+package com.android.example.friendlydetector.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.example.friendlydetector.R;
+
+
+public class VisionResultFragment extends Fragment {
+    private VisionResultFragment() {
+
+    }
+
+    public static VisionResultFragment newInstance() {
+        VisionResultFragment fragment = new VisionResultFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vision_result, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+}
