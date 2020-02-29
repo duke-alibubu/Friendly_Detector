f0a9bed5d1e984baf628bba2a3591c61173071d1
Trinh Dung <c170017@e.ntu.edu.sg>
2020-03-01 03:04:33 +0800

    try using multi threading for this

--- app/src/main/java/com/android/example/friendlydetector/fragments/ViewBookmarksFragment.java
+++ app/src/main/java/com/android/example/friendlydetector/fragments/ViewBookmarksFragment.java
@@ -1,20 +1,40 @@
 package com.android.example.friendlydetector.fragments;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.android.example.friendlydetector.R;
+import com.android.example.friendlydetector.fragments.historyrecyclerview.HistoryItemData;
+import com.android.example.friendlydetector.utils.FirebaseUtils;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 public class ViewBookmarksFragment extends Fragment {
-    private ViewBookmarksFragment() {
+    private static final String LOG_TAG = "History";
+    private static final long ONE_MEGABYTE = 1024 * 1024;
+    private List<HistoryItemData> historyItemDataList = new ArrayList<>();
 
+    private ViewBookmarksFragment() {
     }
 
     public static ViewBookmarksFragment newInstance() {
@@ -32,7 +52,12 @@ public class ViewBookmarksFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_bookmarks, container, false);
+        View root = inflater.inflate(R.layout.fragment_view_bookmarks, container, false);
+
+        loadImagesFromDatabase();
+        for (HistoryItemData item: historyItemDataList)
+            Log.d(LOG_TAG, item.textItem);
+        return root;
     }
 
 
@@ -46,5 +71,71 @@ public class ViewBookmarksFragment extends Fragment {
         super.onDetach();
     }
 
+    private void loadImagesFromDatabase(){
+        String userFolder = FirebaseUtils.getUserEmail();
+
+        Log.d(LOG_TAG, userFolder);
+
+        StorageReference listRef = FirebaseUtils.storage.getReference().child(userFolder);
+
+        listRef.listAll()
+                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                    @Override
+                    public void onSuccess(ListResult listResult) {
+                        for (StorageReference item : listResult.getItems()) {
+                            // All the items under listRef.
+                            historyItemDataList.add(getHistoryItemFromStorageItem(item));
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Uh-oh, an error occurred!
+                        Log.e(LOG_TAG, "Cannot read data!!");
+                    }
+                });
+    }
+
+    private HistoryItemData getHistoryItemFromStorageItem(StorageReference item){
+        final HistoryItemData res = new HistoryItemData();
+        Task textTask = item.getMetadata();
+        textTask.addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
+            @Override
+            public void onSuccess(StorageMetadata storageMetadata) {
+                res.textItem = storageMetadata.getCustomMetadata("text");
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Log.e(LOG_TAG, "Cannot retrieve Metadata");
+
+            }
+        });
+
+        Task imageTask = item.getBytes(ONE_MEGABYTE);
+        imageTask.addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                res.imageItem = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(LOG_TAG, e.getClass().toString() + e.getMessage());
+            }
+        });
+
+        try {
+            Tasks.await(textTask);
+            Tasks.await(imageTask);
+            return res;
+
+        } catch (Exception e){
+            Log.d(LOG_TAG, e.getClass().toString() + e.getMessage());
+            return res;
+        }
+    }
 
 }
